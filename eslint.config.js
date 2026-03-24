70fc59c92265d12f4d831bf0c2b25eee1ac01278
Chaeyeon Won <chaeyeon@Chaeyeons-MacBook-Air.local>
2026-03-25 02:49:41 +0900

    fix: exclude api/ from eslint (node globals conflict)

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'legacy']),
+  globalIgnores(['dist', 'legacy', 'api']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
